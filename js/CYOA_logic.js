481690c77c6fc1007894c730690f3d368d666ffc
Indigo253931 <indigo253931@gmail.com>
2016-07-01 08:56:13 -0600

    cozumel js

--- js/CYOA_logic.js
+++ js/CYOA_logic.js
@@ -1,14 +1,15 @@
 //Options for Cozumel file 
+
 $(document).ready (function(){
 
 Cozumel_Intro = {
 html: '<div id="Cozumel_Intro">\
-        <p class="transient">You decide to go to Cozumel.<br> As you arrive and arrive to the terminal, a man wearing large sunglasses holds a sign with your name on it.<br>You approach him and ask what is going on.</p>\
-        <p class="transient">He responds “Aqui esta tu paquete, y buena suerte.” and hands you a package, disappearing into the crowd.</p>\
-        <p class="transient">Inside the package is a map of Cozumel, a pair of car keys, and directions to a diveshop.</p>\
-        <p class="transient">The instructions say you must figure out how to complete three dives in a 24 period, including a dive of 80 feet.<br> You have never learned to swim, not to mention scuba-dive. And you are aware of the dangers of a gas embolism, when one or more gas bubbles\
+        <p>You decide to go to Cozumel.<br> As you arrive and arrive to the terminal, a man wearing large sunglasses holds a sign with your name on it.<br>You approach him and ask what is going on.</p>\
+        <p>He responds “Aqui esta tu paquete, y buena suerte.” and hands you a package, disappearing into the crowd.</p>\
+        <p>Inside the package is a map of Cozumel, a pair of car keys, and directions to a diveshop.</p>\
+        <p>The instructions say you must figure out how to complete three dives in a 24 period, including a dive of 80 feet.<br> You have never learned to swim, not to mention scuba-dive. And you are aware of the dangers of a gas embolism, when one or more gas bubbles\
         enter a vein or artery. It can be lethal.</p>\
-        <p class="transient">You arrive to the dive shop and ask for advice. Now you need to decide: do you <a href="#" id="Choose_To_Dive_Link">face your fears</a> or <a href="#" id="Back_Out_Link"> back out?</a></p></div>'};
+        <p>You arrive to the dive shop and ask for advice. Now you need to decide: do you <a href="#" id="Choose_To_Dive_Link">face your fears</a> or <a href="#" id="Back_Out_Link"> back out?</a></p></div>'};
 
 Back_Out = {
 html: ' <div id="Back_Out">\ <p class="transient">You are too afraid of drowning and decide not to back out. You recieve a phone call. A recording tells you your loved one has died."</p>\</div>'
@@ -24,7 +25,7 @@ html: '<div id="Choose_To_Dive">\
         <p class="transient">You somehow mangage to get up and climb into the Zodiac. <br> The boat moves into the horizon. <br>The instructor throws an anchor into the water and the two of you dive in.</p>\
         <p class="transient">He immediately swims towards the reef. As you approach it, the diversity of the ocean overwhelms you.<br> The colors and shape of the Santa Rosa reef become more defined.</p>\
         <p class="transient">About 20 minutes into the dive, the intructor signals to follow after a sea turtle.<br>\
-        You decide to <a href="#" id="Get_Lost_Link">Get lost in the ocean</a> or <a href="#" id=Follow_Turtle_Link">follow after the wild sea\
+        You decide to <a href="#" id="Get_Lost_Link">Get lost in the ocean</a> or <a href="#" id="Follow_Turtle_Link">follow after the wild sea\
         creature.</a></p>\
         </div>'};
 
@@ -32,7 +33,7 @@ Follow_Turtle = {
     html: '<div id="Follow_Turtle"><p class="transient">You follow after the instrutor, who is holding onto the turtles shell and prompting you do the same.<br>In a dreamlike state, you grab with on one hand at the top, and the other at the bottom.<br> Without notice, the instructor lets his hands go as you are being pullled at high speed to the surface.<br>You snap back to reality and let the turtle go. You swim back to the Zodiac.</p>\
         <p class="transient">After a meal and a few hours rest, it is time for the final dive; a night dive in\
         Chankanaab.<br>The lagoon opens up to the ocean, illuminated by the blue bioluminescence and moonlight.</p>\
-        <p class="transient">The instructor waits for you on the pier with two tanks, ready to go.<br>\ For the last time, he hands over the weight belt.<br>\ Suddenly, you realize that he is the same man who gave you the package and wished you good luck.<br>\ You switch on your flashlight, feeling oddly at ease, and <a href="#Final_Dive">join him in the lagoon.</a></p>\ </div>'};       
+        <p class="transient">The instructor waits for you on the pier with two tanks, ready to go.<br>\ For the last time, he hands over the weight belt.<br>\ Suddenly, you realize that he is the same man who gave you the package and wished you good luck.<br>\ You switch on your flashlight, feeling oddly at ease, and <a href="#" id="Final_Dive_Link">join him in the lagoon.</a></p>\ </div>'};       
 
 Get_Lost={
     html:'<div id="Get_Lost"><p class="Player_Loses_Cozumel">You choose not to follow after the turtle. Not knowing where you are, you decide to resurface. Unable to find your way, the current pulls you to the Carribean sea. Eventually you drown.</p></div>'};
@@ -58,37 +59,59 @@ Final_Dive = {
 html: $("#Get_Lost") 
 };*/
 
-console.log(Player_Loses_Cozumel);
 
-$("body").append(Cozumel_Intro.html);
+var a = [];
+a.push(Cozumel_Intro);
+a.push(Back_Out);
+a.push(Choose_To_Dive);
+a.push(Follow_Turtle);
+a.push(Get_Lost);
+a.push(Player_Loses_Cozumel);
+a.push(Player_Wins_Cozumel);
+a.push(Final_Dive);
+
+for (var i = 0; i < a.length; i++) {
+
+$("body").append(a[i].html);
+}
+
+
+$("div").hide();
+
+$("#Cozumel_Intro").show();
+
+//$(!"div[display]").hide();
+//$(a[i])
 
 $("#Choose_To_Dive_Link").click(function(){
     $("#Cozumel_Intro").hide();
-    $("body").append(Choose_To_Dive.html);
+    $("#Choose_To_Dive").show();
 });
+
 $("#Back_Out_Link").click(function(){
     $("#Cozumel_Intro").hide();
-    $("body").append(Back_Out.html);
+    $("#Back_Out").show();
 });
 
 $("#Follow_Turtle_Link").click(function(){
     $("#Choose_To_Dive").hide();
-    $("body").append(Back_Out_Link.html);
+     $("#Follow_Turtle").show();
 });
+
 $("#Get_Lost_Link").click(function(){
     $("#Choose_To_Dive").hide();
-    $("body").append(Player_Loses_Cozumel_Link.html);
+     $("#Get_Lost").show();
 });
 
 $("#Final_Dive_Link").click(function(){
     $("#Follow_Turtle").hide();
-    $("body").append(Follow_Turtle.html);
-});
-$("#Player_Wins_Cozumel_Link").click(function(){
-    $("#Follow_Turtle").hide();
-    $("body").append(Follow_Turtle.html);
+    $("#Player_Wins_Cozumel").show();
 });
 
+
+console.log(Follow_Turtle);
+console.log(Choose_To_Dive);
+console.log(Player_Wins_Cozumel);
 
 /*$("#show").click(function(){
     $("p").show();

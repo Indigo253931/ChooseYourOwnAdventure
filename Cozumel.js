e066c930e66fffc5ba19524c5d57462e08aafb69
Indigo253931 <indigo253931@gmail.com>
2016-07-10 17:52:41 -0600

    added images

--- Cozumel.js
+++ Cozumel.js
@@ -5,43 +5,91 @@ console.log("hello");
 
   var body = document.querySelector("body");
   var beginning_content = document.createElement("div");
-
   var introParagraph = document.createElement("P");
-  introParagraph.textContent = "You decide to go to Cozumel. As you arrive to the terminal, a man wearing large sunglasses holds a sign with your name on it.<br>";
-
+  
+  //beginning paragraphs 
+  introParagraph.textContent = "You decide to go to Cozumel. As you arrive to the terminal, a man wearing large sunglasses holds a sign with your name on it.";
   beginning_content.appendChild(introParagraph);
   body.appendChild(beginning_content);
   
+  var map_pic = document.createElement("IMG");
+  map_pic.src="media/Cozumel.jpg";
+  introParagraph.appendChild(map_pic);
 
   var p2 = document.createElement("P");
   p2.textContent="You approach him and ask 'What is going on?' He responds, 'Aqui esta tu paquete, y buena suerte.' and hands over a package before disappearing into the crowd.";
   beginning_content.appendChild(p2);
-
 
   var p3 = document.createElement("P");
   p3.textContent="Inside the package is a map of Cozumel, a pair of car keys, and directions to a diveshop. The instructions say you must figure out how to complete three dives in a 24 period, including a dive of 80 feet.";
   beginning_content.appendChild(p3);
 
   var p4 = document.createElement("P");
-  p4.textContent="You have never learned to swim, not to mention scuba-dive. <br>And you are aware of the dangers of an air embolism, when one or more gas bubbles enter a vein or artery. <br>It can be lethal.";
+  p4.textContent="You have never learned to swim, not to mention scuba-dive. <br>And you are aware of the dangers of an air embolism, when one or more gas bubbles enter a vein or artery. It can be lethal.";
   beginning_content.appendChild(p4);
   var embolism = document.createElement("IMG");
   embolism.src="media/air_embolism.jpg";
   p4.appendChild(embolism);
 
   var p5 = document.createElement("P");
-  p5.textContent="You arrive to the dive shop. Do you <a href='#' id='Choose_To_Dive_Link'>face your fears</a> or 
-//   // <a href="#" id="Back_Out_Link"> back out?";
+  p5.textContent="You arrive to the dive shop. Do you <a href='#' id='Choice_1'>face your fears</a> or <a href='id=Choice_2> back out?";
 
-  var p6 = document.createElement("P");
-  p6.textContent="";
+  //first choice (choose to dive)
+  //create new div
+  var ch1 = document.createElement("div");
+  var ch1_p1 = document.createElement("P");
+  ch1_p1.textContent="You decide to dive, despite your fear of drowning. You try your best to follow the directions of the scuba instructor. You first put the tank on the seat and sit down.";
+  ch1.appendChild(ch1_p1);
+  body.appendChild(ch1);
 
-  var p7 = document.createElement("P");
-  p7.textContent="";
+//   var ch1_p2 = document.createElement("P");
+//   ch1_p2.textContent=
 
-  var p8 = document.createElement("P");
-  p8.textContent="";
+
+//   var ch1_p3 = document.createElement("P");
+// ch1_p2.textContent=
+
+//   var ch1_p4 = document.createElement("P");
+
+//   ch1_p2.textContent=
+//   var ch1_p5 = document.createElement("P");
+  // var ch1_p2=document.createElement("P");
+  // p2.textContent="Next, you buckle the Boyuncy Control vest and put the regulator in your mouth.<br><img src='media/bc_vest.jpg' style='height:200px;width:300px;'/><img src='media/regulator.jpg' style='height:200px;width:300px'br>Then, you pull on your fins and mask.<br><img src='media/mask.jpg' style='height:200px;width:300px;'/><img src='media/fins.jpg' style='height:200px;width:300px;'/><br>Holding the regulator and mask with one hand, you use your other hand to push backwards into the open water.";
+  // // var choice1_p3=document.createElement("P");
+  // p3.textContent="";
+  //  var choice1_p4=document.createElement("P");
+  // p4.textContent="";
+  //  var choice1_p5=document.createElement("P");
+  // p5.textContent="";
+  //  var Choice1_p6=document.createElement("P");
+  // p6.textContent="";
+  //  var Choice1_p7=document.createElement("P");
+  // p7.textContent="";
+
+var choice2 = document.createElement("div");
+var choice2_p2 = document.createElement("P");
+  p1.textContent="You are too afraid of drowning and decide not to back out. You recieve a phone call. A recording tells you your loved one has died.";
+
+
   
+//BUTTON DISPLAYS NUMBER OF TIMES PLAYED
+var btn = document.getElementById("btn");
+var counter  = document.getElementById("btn-counter");
+counter.innerHTML = 0;
+btn.onclick = function(){
+  counter.innerHTML++;
+};
+
+// play_again.innerHTML = "You have played " + clicks + "times!";
+// body.appendChild(play_again);
+
+// console.log("play_again")
+// play_again.onclick = function() {
+// clicks++;
+// console.log("You have played " + clicks + "times!");
+// };
+
+
 
   };
      
@@ -67,21 +115,6 @@ console.log("hello");
 //  image.height="";
 
 // });
-  
-
-
-//BUTTON DISPLAYS NUMBER OF TIMES PLAYED
-var clicks = 0;
-var play_again = document.createElement("button");
-body.appendChild(play_again);
-var button_text= document.createElement("p");
-button.appendChild(button_text);
-button_text.textContent="You have played " + clicks + "times!";
-
-play_again.onclick = function() {
-clicks++;
-console.log("You have played " + clicks + "times!");
-};
 
 // //Cozumel_Intro = {
 // //html: '<div id="Cozumel_Intro">\<p class="text_border">You decide to go to Cozumel.<br><img src="media/Cozumel.jpg" style="height:200px;width:300px;"/><br> As you arrive to the terminal, a man wearing large sunglasses holds a sign with your name on it.<br>You approach him and ask "What is going on?"</p>\<p class="text_border"></p>\<p class="text_border">Inside the package is a map of Cozumel, a pair of car keys, and directions to a diveshop.<br> <p class="text_border">You arrive to the dive shop. <br> <img src="media/diveshop.png" style="height:200px;width:300px;"/></p>\<br><p class="text_border">The instructions say you must figure out how to complete three dives in a 24 period, including a dive of 80 feet.<br> You have never learned to swim, not to mention scuba-dive. <br>And you are aware of the dangers of an air embolism, when one or more gas bubbles enter a vein or artery.<br> <img src="media/air_embolism.jpg" style="height:200px;width:300px;/><br>It can be lethal.</p>\<p class="text_border"><br>Do you <a href="#" id="Choose_To_Dive_Link">face your fears</a> or <a href="#" id="Back_Out_Link"> back out?</a></p></div>'};
@@ -91,7 +124,7 @@ console.log("You have played " + clicks + "times!");
 // //html: '<div id="Back_Out">\<p class="text_border">You are too afraid of drowning and decide not to back out. You recieve a phone call. A recording tells you your loved one has died."<br> Click <a href="CYOA_intro.html"> here </a> to play again.</p>\</div>'};
 
 // //Choose_To_Dive = {
-// //html: '<div id="Choose_To_Dive">\<p class="text_border">You decide to dive, despite your fear of drowning. <br> <img src="media/wave.gif" style="height:200px;width:300px;"/> <br>You try your best to follow the directions of the scuba instructor.<br>You first put the tank on the seat and sit down. <br> <img src="media/tanks.jpg" style="height:200px;width:300px;"/> <br> Next, you buckle the Boyuncy Control vest and put the regulator in your mouth.<br><img src="media/bc_vest.jpg" style="height:200px;width:300px;"/><img src="media/regulator.jpg" style="height:200px;width:300px;"/><br>Then, you pull on your fins and mask.<br><img src="media/mask.jpg" style="height:200px;width:300px;"/><img src="media/fins.jpg" style="height:200px;width:300px;"/><br>Holding the regulator and mask with one hand, you use your other hand to push backwards into the open water.</p>\<p class="text_border"><img src="media/water.gif" style="height:200px;width:300px;"/><br>As you descend headfirst into the Palancar Reef, you can see the blue abyss of the ocean.<br>A Bluefin tuna swims past at 45mph, reflecting the light of the sea.<br> <img src="media/bluefin.gif" style="height:200px;width:300px;"/><img src="media/tuna.gif" style="height:200px;width:300px;"/><br>The farther down you go, the reef appears darker, and less colorful.<br>You can hear yourself breathing. <br><img src="media/giphy.gif" style="height:200px;width:300px;"/><br>Before you realize it, you are already 80 feet deep. The instructor gives you a thumbs-up, a signal to return to the surface.</p>\<p class="text_border">As you get back into the boat, you are unsure if you can make another dive. The combination of fear and jet lag left you exhausted.<br>The instructor reminds you to meet back in two hours for a 50-foot dive.</p>\<p class="text_border">You fall asleep right there on the sand. <br> <img src="media/sand.gif" style="height:200px;width:300px;/><br>The instructor wakes you up, hands you a weight-belt, and says "We are going to Santa Rosa".</p>\<p class="text_border">You somehow mangage to get up and climb into the Zodiac. <br> The boat moves into the horizon. <br><br> <img src="media/sunset_clouds.gif" style="height:200px;width:300px;"/> <img src="media/sunsent gif.gif" style="height:200px;width:300px;"/> <br>The instructor throws an anchor into the water and the two of you dive in.</p>\<p class="text_border">He immediately swims towards the reef. As you approach it, the colors and shape of the Santa Rosa reef become more defined, and the oceans diversity overwhelms you.<br> <img src="media/reef.gif" style="height:200px;width:300px;/><br> <img src="media/gereen_coral.gif" style="height:200px;width:300px;/><br></p>\<p class="text_border">About 20 minutes into the dive, the intructor signals to follow after a sea turtle.<br>You decide to <a href="#" id="Get_Lost_Link">Get lost in the ocean</a> or <a href="#" id="Follow_Turtle_Link">follow after the wild sea creature.</a></p>\</div>'};
+// //html: '<div id="Choose_To_Dive">\<p class="text_border">You decide to dive <img src="media/tanks.jpg" style="height:200px;width:300px;"/> <br> /p>\<p class="text_border"><img src="media/water.gif" style="height:200px;width:300px;"/><br>As you descend headfirst into the Palancar Reef, you can see the blue abyss of the ocean.<br>A Bluefin tuna swims past at 45mph, reflecting the light of the sea.<br> <img src="media/bluefin.gif" style="height:200px;width:300px;"/><img src="media/tuna.gif" style="height:200px;width:300px;"/><br>The farther down you go, the reef appears darker, and less colorful.<br>You can hear yourself breathing. <br><img src="media/giphy.gif" style="height:200px;width:300px;"/><br>Before you realize it, you are already 80 feet deep. The instructor gives you a thumbs-up, a signal to return to the surface.</p>\<p class="text_border">As you get back into the boat, you are unsure if you can make another dive. The combination of fear and jet lag left you exhausted.<br>The instructor reminds you to meet back in two hours for a 50-foot dive.</p>\<p class="text_border">You fall asleep right there on the sand. <br> <img src="media/sand.gif" style="height:200px;width:300px;/><br>The instructor wakes you up, hands you a weight-belt, and says "We are going to Santa Rosa".</p>\<p class="text_border">You somehow mangage to get up and climb into the Zodiac. <br> The boat moves into the horizon. <br><br> <img src="media/sunset_clouds.gif" style="height:200px;width:300px;"/> <img src="media/sunsent gif.gif" style="height:200px;width:300px;"/> <br>The instructor throws an anchor into the water and the two of you dive in.</p>\<p class="text_border">He immediately swims towards the reef. As you approach it, the colors and shape of the Santa Rosa reef become more defined, and the oceans diversity overwhelms you.<br> <img src="media/reef.gif" style="height:200px;width:300px;/><br> <img src="media/gereen_coral.gif" style="height:200px;width:300px;/><br></p>\<p class="text_border">About 20 minutes into the dive, the intructor signals to follow after a sea turtle.<br>You decide to <a href="#" id="Get_Lost_Link">Get lost in the ocean</a> or <a href="#" id="Follow_Turtle_Link">follow after the wild sea creature.</a></p>\</div>'};
 
 // //Follow_Turtle = {
 //   //  html: '<div id="Follow_Turtle"><p class="text_border">You follow after the instrutor, who is holding onto the turtles shell and prompting you do the same.<br>In a dreamlike state, you grab with on one hand at the top, and the other at the bottom.<br> Without notice, the instructor lets his hands go as you are being pullled at high speed to the surface.<br>You snap back to reality and let the turtle go. You swim back to the Zodiac.</p>\<p class="text_border">After a meal and a few hours rest, it is time for the final dive; a night dive in Chankanaab.<br>The lagoon opens up to the ocean, illuminated by the blue bioluminescence and moonlight.</p>\<p class="text_border">The instructor waits for you on the pier with two tanks, ready to go.<br>\ For the last time, he hands over the weight belt.<br>\ Suddenly, you realize that he is the same man who gave you the package and wished you good luck.<br>\ You switch on your flashlight, feeling oddly at ease, and <a href="#" id="Final_Dive_Link">join him in the lagoon.</a></p>\ </div>'};       
